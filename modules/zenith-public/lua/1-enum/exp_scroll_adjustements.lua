23a924a071893838b6363da2b2a61987c998b86a
TMP314 <15794042+TMP314@users.noreply.github.com>
2025-07-14 00:47:53 +0300

    Updated lua formatting

--- modules/zenith-public/lua/1-enum/exp_scroll_adjustements.lua
+++ modules/zenith-public/lua/1-enum/exp_scroll_adjustements.lua
@@ -3,12 +3,12 @@
 local m = Module:new('exp_scroll_adjustements')
 
 -- Override the experience gained from Page from the Dragon Chronicles to 1000-1500
-m:addOverride('xi.items.page_from_the_dragon_chronicles.onItemUse', function(target)   
+m:addOverride('xi.items.page_from_the_dragon_chronicles.onItemUse', function(target)
     target:addExp(xi.settings.main.EXP_RATE * math.random(1000, 1500))
 end)
 
 -- Override the experience gained from Page from Miratetes Memoirs to 1250-2000
-m:addOverride('xi.items.page_from_miratetes_memoirs.onItemUse', function(target)   
+m:addOverride('xi.items.page_from_miratetes_memoirs.onItemUse', function(target)
     target:addExp(xi.settings.main.EXP_RATE * math.random(1250, 2000))
 end)
 
