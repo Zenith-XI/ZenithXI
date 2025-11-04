4b237e65f11b2933735f8d0a54d26e5d3a876baf
NathanielCFisher <76634100+NathanielCFisher@users.noreply.github.com>
2025-11-04 13:54:08 +0100

    refactor with IN block

--- modules/zenith-public/sql/spellChanges.sql
+++ modules/zenith-public/sql/spellChanges.sql
@@ -9,13 +9,15 @@
 
 -- Absorb spells' cast time reverted to pre-August 12, 2014 update values
 
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-acc';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-attri';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-str';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-dex';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-vit';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-agi';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-int';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-mnd';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-chr';
-UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` = 'absorb-tp';
+UPDATE `spell_list` SET `castTime` = 2000 WHERE `name` IN (
+	'absorb-acc',
+	'absorb-attri',
+	'absorb-str',
+	'absorb-dex',
+	'absorb-vit',
+	'absorb-agi',
+	'absorb-int',
+	'absorb-mnd',
+	'absorb-chr',
+	'absorb-tp'
+);
